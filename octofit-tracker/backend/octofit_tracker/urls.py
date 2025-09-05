0b8e6a93746bf142812aa12e646ab969a5090c24
pblerial <dperial44@gmail.com>
2025-09-05 18:50:32 +0000

    Réorganisation des routes URL : suppression de la vue home redondante et ajustement des chemins d'API.

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
-
-def home_view(request):
-    return HttpResponse("Welcome to Octofit Tracker!")
+from .views import home_view
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
-    path('', include('octofit_tracker.api_urls')),
-    path('home/', home_view, name='home'),
+    path('api/', include('api_urls')),
 ]
