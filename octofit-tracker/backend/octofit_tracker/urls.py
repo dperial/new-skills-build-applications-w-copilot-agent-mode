0a84cd6b82395fc3d1570a9e7fee8ec11757a41f
pblerial <dperial44@gmail.com>
2025-09-05 20:34:00 +0000

    Correction du chemin d'importation pour les URLs de l'API

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -5,5 +5,5 @@ from octofit_tracker.views import home_view
 urlpatterns = [
     path('', home_view, name='home'),
     path('admin/', admin.site.urls),
-    path('api/', include('octofit_tracker.api_urls')),
+    path('api/', include('api.urls')),  # Chemin relatif, pas d’URL absolue
     ]
